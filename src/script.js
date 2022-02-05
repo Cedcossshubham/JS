570577991c888e0d4d368cc82902b723d6096dc2
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 15:37:26 +0530

    use else if

--- src/script.js
+++ src/script.js
@@ -585,3 +585,22 @@ function nextInLine(arr, item) {
   }
   
  console.log( testElse(4));
+
+
+ //Else If Statements
+
+ function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    }
+  
+    else if (val < 5) {
+      return "Smaller than 5";
+    }
+    else{
+  
+    return "Between 5 and 10";
+    }
+  }
+  
+ console.log(testElseIf(7)); 
