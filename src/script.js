72444ad59d677d03f4544ad8b307ddcb5b19aedd
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 16:22:47 +0530

    Return Early Pattern for Functions

--- src/script.js
+++ src/script.js
@@ -802,3 +802,16 @@ function caseInSwitch(val) {
   }
   
   console.log(isLess(10, 15));
+
+
+  //new program
+  function abTest(a, b) {
+  
+    if(a<0 || b<0)
+      return undefined;
+
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+ console.log( abTest(2,2));
