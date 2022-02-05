2fa072b037b8ff4b4e28e624654cfdded5012b65
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 13:44:30 +0530

    global vs local scope

--- src/script.js
+++ src/script.js
@@ -334,3 +334,17 @@ function myLocalScope() {
   myLocalScope();
  
   console.log('outside myLocalScope', myVar);
+
+  //global vs local scope
+  // Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+console.log(myOutfit());
