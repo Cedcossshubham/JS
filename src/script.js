f448b86cc36b1e4499517474a9e9f9fc5f0244f4
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 13:35:21 +0530

    global and local var

--- src/script.js
+++ src/script.js
@@ -298,3 +298,28 @@ function reusableFunction(){
   console.log(timesFive(5));
   timesFive(10);
   timesFive(0);
+
+
+
+  //global var 
+let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+fun1();
+fun2();
