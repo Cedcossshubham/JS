bf73386148fe0cc87c70cc8414f01d5a89ecb0ad
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 15:19:05 +0530

    Comparison with the Less Than Operator

--- src/script.js
+++ src/script.js
@@ -504,3 +504,18 @@ function nextInLine(arr, item) {
   }
   
   console.log(testGreaterOrEqual(10));
+
+//Comparison with the Less Than Operator
+  function testLessThan(val) {
+    if (val<25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val<55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  console.log(testLessThan(10));
