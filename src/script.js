a661da429f59474577d8b099241c17a4a6ae7aac
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 17:14:08 +0530

    look up

--- src/script.js
+++ src/script.js
@@ -941,3 +941,25 @@ const myDog = {
 
 // Only change code below this line
 delete myDog.tails;
+
+//Using Objects for Lookups
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+  const alpha = {
+    "alpha":"Adams",
+    "bravo":"Boston",
+    "charlie":"Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot":"Frank"
+  };
+  
+  result = alpha[val];
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
