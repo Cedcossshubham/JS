8731908829bc4f1eb226a8fabbecce7827fc9e57
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 15:40:47 +0530

    else if

--- src/script.js
+++ src/script.js
@@ -604,3 +604,17 @@ function nextInLine(arr, item) {
   }
   
  console.log(testElseIf(7)); 
+
+
+ //if else
+ function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+  
+  console.log(orderMyLogic(7));
