110666d342335aa8d7ac659d5e64d45b1ffd53ce
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 15:45:24 +0530

    if else chain

--- src/script.js
+++ src/script.js
@@ -618,3 +618,23 @@ function nextInLine(arr, item) {
   }
   
   console.log(orderMyLogic(7));
+
+
+  function testSize(num) {
+  //if else chain
+  if(num<5)
+  return "Tiny";
+else if(num < 10)
+  return "Small";
+else if(num<15)
+  return "Medium";
+else if(num<20)
+  return "Large";
+else if(num>=20)
+  return "Huge";
+else
+  return "Change Me";
+  
+}
+
+console.log(testSize(7));
