cf6b3bdc0755697d90b151dbb48c15a636ce8a26
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 15:10:56 +0530

    Comparison with the Greater Than Operator

--- src/script.js
+++ src/script.js
@@ -474,3 +474,18 @@ function nextInLine(arr, item) {
   }
   
   console.log(testStrictNotEqual(10));
+
+//Comparison with the Greater Than Operator
+  function testGreaterThan(val) {
+    if (val>100) {  
+      return "Over 100";
+    }
+  
+    if (val>10) {  
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+ console.log(testGreaterThan(10));
