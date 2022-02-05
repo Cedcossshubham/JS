db35efc1f1ef213423965c334f180b7755549b9b
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 16:56:23 +0530

    access object

--- src/script.js
+++ src/script.js
@@ -866,3 +866,16 @@ const myDog = {
   tails:1,
   friends:["tommy"]
 };
+
+
+//access obj value
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
