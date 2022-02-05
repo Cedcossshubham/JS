6f30e335f7c924eb5696392787cbeee44db1fbec
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 17:04:35 +0530

    change obj property

--- src/script.js
+++ src/script.js
@@ -904,3 +904,16 @@ const testObj = {
 // Only change code below this line
 const playerNumber = 16;  // Change this line
 const player = testObj[playerNumber];   // Change this line
+
+
+//change obj property
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog["name"]="Happy Coder";
