bf3984424c07ee8f9238eb461e4c0faf319fe57e
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 10:55:57 +0530

    initialise uninitialized var

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ this is a multiline comment
 
 var myName;  //variable created my name
 
-
+/*
 var a = 7;  // define a variable with val 7
 var b;
 
@@ -14,6 +14,20 @@ a = 7;
 // assign value of var a to b
 b =a;
 
+*/
+
 //define a string var
 var myFirstName ="John";
 var myLastName = "Doe";
+
+
+
+//change  the value of var
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
