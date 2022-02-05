606867ac77f0c3dd658b67c915017202c76518f9
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 17:08:28 +0530

    delete property

--- src/script.js
+++ src/script.js
@@ -927,3 +927,17 @@ const myDog = {
 };
 
 myDog.bark = "woof";
+
+
+//delete property
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails;
