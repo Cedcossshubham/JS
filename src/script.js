5a4eeee106fba3451e05e6bb03227ccf4b911ef4
cedcoss <shubhambajpai@cedcoss.com>
2022-02-05 16:00:55 +0530

    default case

--- src/script.js
+++ src/script.js
@@ -702,3 +702,33 @@ function caseInSwitch(val) {
   }
   
   console.log(caseInSwitch(1));
+
+
+  //default in switch
+  function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+  
+    switch (val) {
+    case "a":
+      answer="apple";
+      break;
+    case "b":
+      answer="bird";
+      break;
+      case "c":
+      answer="cat";
+      break;
+      
+  
+  
+    default:
+         answer="stuff";
+      break;
+  }
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  console.log(switchOfStuff(1));
